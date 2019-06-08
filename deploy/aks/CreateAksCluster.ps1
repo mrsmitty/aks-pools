f0292ef3e8ad98115babab18ba54d1e141a2fb87
Paul Smithdale <psmithdale@gmail.com>
2019-06-08 12:17:07 +0800

    Update scripts for VMSS and MultiAgentpool

--- deploy/aks/CreateAksCluster.ps1
+++ deploy/aks/CreateAksCluster.ps1
@@ -1,18 +1,23 @@
 <#
 .SYNOPSIS
 .PARAMETER winpassword
-.LINK
+.NOTES
+.NOTES
+Create a valid windows password using the following
+^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%\\^&\\*\\(\\)])[a-zA-Z\\d!@#$%\\^&\\*\\(\\)]{12,123}$
+https://www.browserling.com/tools/text-from-regex
 #>
 
 param (
-    [securestring] $winPassword,
-    [string] $resourceGroup = "paul-rg-aue-akspools"
+    [string] $winPassword,
+    [string] $resourceGroup = "paul-rg-aue-akspools",
+    [string] $clusterName = "paul-aks-aue-winpool"
 )
 az group create --name $resourceGroup --location australiaeast
 
 az aks create `
     --resource-group $resourceGroup `
-    --name myAKSCluster `
+    --name $clusterName `
     --node-count 1 `
     --enable-addons monitoring `
     --kubernetes-version 1.14.0 `
@@ -21,3 +26,16 @@ az aks create `
     --windows-admin-username azureuser `
     --enable-vmss `
     --network-plugin azure
+
+    az aks nodepool add `
+    --resource-group $resourceGroup `
+    --cluster-name $clusterName `
+    --os-type Windows `
+    --name npwin `
+    --node-count 1 `
+    --kubernetes-version 1.14.0
+
+    # only run if kubectl is not installed
+    #az aks install-cli
+
+    az aks get-credentials --resource-group $resourceGroup --name $clusterName
--- deploy/aks/RegisterAksWindowsPreview.ps1
+++ deploy/aks/RegisterAksWindowsPreview.ps1
@@ -1,18 +1,30 @@
 <#
 .SYNOPSIS
-Registeres the AKS WindowsPreview features in the Subscription
+Registeres the AKS WindowsPreview, MultiAgentpoolPreview, VMSSPreview features in the Subscription
+
 .LINK
 https://docs.microsoft.com/en-us/azure/aks/windows-container-cli
+https://docs.microsoft.com/en-au/azure/aks/use-multiple-node-pools
 #>
+
+function checkFeature($featureName) {
+    do {
+        $feature = az feature list -o json --query "[?contains(name, '$featureName')].{Name:name,State:properties.state}" | ConvertFrom-Json
+        Write-Host $feature.State
+        Start-Sleep -Seconds 5
+    } while ($feature.State -ne 'Registered')
+}
 
 az extension add --name aks-preview
 
 az feature register --name WindowsPreview --namespace Microsoft.ContainerService
+checkFeature('Microsoft.ContainerService/WindowsPreview')
+az provider register --namespace Microsoft.ContainerService
 
-do {
-    $feature = az feature list -o json --query "[?contains(name, 'Microsoft.ContainerService/WindowsPreview')].{Name:name,State:properties.state}" | ConvertFrom-Json
-    Write-Host $feature.State
-    Start-Sleep -Seconds 5
-} while ($feature.State -ne 'Registered')
+az feature register --name MultiAgentpoolPreview --namespace Microsoft.ContainerService
+checkFeature('Microsoft.ContainerService/MultiAgentpoolPreview')
+az provider register --namespace Microsoft.ContainerService
 
+az feature register --name VMSSPreview --namespace Microsoft.ContainerService
+checkFeature('Microsoft.ContainerService/VMSSPreview')
 az provider register --namespace Microsoft.ContainerService
